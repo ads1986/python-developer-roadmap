9172693a5b71def3774a90a9e4dc4f966450290a
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-06 15:35:28 -0300

    Update variables.py

--- language-basics/variables.py
+++ language-basics/variables.py
@@ -1,94 +1,107 @@
 # Font : https://www.w3schools.com/python/python_variables.asp
+
+print("#creating variables")
 x = 5
 y = "John"  # Same as 'John'
 print(x)
 print(y)
+print()
 
+print("#same variables but diferent types")
 x = 4
 x = "Sally"
 print(x)
+print()
 
+print("#casting")
 x = str(3)
 y = int(3)
 z = float(3)
 print(x)
 print(y)
 print(z)
+print()
 
+print("#casting")
 x = 5
 y = "John"
 print(type(x))
 print(type(y))
+print(z)
+print()
 
+print("##A will not overwrite a (case sensitive)")
 a = 4
 A = "Sally"
-#A will not overwrite a (case sensitive)
+print()
 
-# variable naming
-
+print("#variable naming")
 myvar = "John"
 my_var = "John"
 _my_var = "John"
 myVar = "John"
 MYVAR = "John"
 myvar2 = "John"
+print()
 
-# illegal variable names
+print("#illegal variable names")
+print("# 2myvar = 'John'")
+print("# my-var = 'John'")
+print("# my var = 'John'")
+print()
 
-# 2myvar = "John"
-# my-var = "John"
-# my var = "John"
-
-# Assigning multiple values
-
+print("#assigning multiple values")
 x, y, z = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
+print()
 
-# One value to multiple variables
+print("# One value to multiple variables")
 x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
+print()
 
-# Unpacking collection
-
+print("#unpacking collection")
 fruits = ["apple", "banana", "cherry"]
 x, y, z = fruits
 print(x)
 print(y)
 print(z)
+print()
 
-# unpacking tuple
+print("#unpacking tuple")
 fruits = ("apple", "banana", "cherry")
 (green, yellow, red) = fruits
-
 print(green)
 print(yellow)
 print(red)
+print()
 
-# Output variables
-
+print("#output variables")
 x = "awesome"
 print("Python is " + x)
-
 x = "Python is "
 y = "awesome"
 z = x + y
 print(z)
+print()
 
+print("#output variables - sum")
 x = 5
 y = 10
 print(5 + 10)
+print()
 
-# illegal sum string with value
+print("#illegal sum string with value")
+print("# x = 5")
+print("# y = 'John'")
+print("# print(x + y)")
+print()
 
-# x = 5
-# y = "John"
-# print(x + y)
-
-# Globa Variable
+print("#globa Variable")
 x = "awesome"
 
 def myFunc():
@@ -105,9 +118,9 @@ def myFunc():
 myFunc()
 
 print("Python is " + x)
+print()
 
-# Global scope
-
+print("#global scope")
 def myFunc():
     global x
     x = "Fantastic"
@@ -125,3 +138,4 @@ def myFunc():
 myFunc()
 
 print("Python is " + x)
+print()
