471f8932451771571ef4a8de582f6869d3e1dce5
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-06 17:23:12 -0300

    Update numbers-operators-precedence.py

--- language-basics/numbers-operators-precedence.py
+++ language-basics/numbers-operators-precedence.py
@@ -1,1 +1,33 @@
+#Fonts : https://www.w3schools.com/python/python_numbers.asp
+         https://www.w3schools.com/python/python_operators.asp
 
+print("Numbers")
+
+print("#int") # Whole number, positive or negative, without decimals of unlimited length
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+print()
+
+print("#float") # Positive or negative, containing one or more decimals
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+print()
+
+print("#float2") # Can be scientific number with an "e" to indicate the power of 10
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+print()
+
+print("Operators")
