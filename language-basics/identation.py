20ac49bec23d2dda5ad90fd6ca8c7c1e2c9227a0
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-07 17:45:40 -0300

    Update identation.py

--- language-basics/identation.py
+++ language-basics/identation.py
@@ -1,1 +1,31 @@
+#Font : https://www.w3schools.com/python/python_syntax.asp
 
+print("Identation")
+print()
+
+print("#Block of code")
+
+if 5 > 2:
+    print("Five is greater thant two!")
+    
+print()
+
+print("#Invalid Identation")
+
+#if 5 > 2:
+#print("Five is greater thant two!")
+
+print("#Number of Space")
+
+if 5 > 2:
+        print("Five is greater thant two! (with 2 spaces)") # have to be at least one space
+
+print()
+
+print("#Same number of space in same block")
+
+#if 5 > 2:
+    #print("Five is greater thant two!")
+        #print("Five is greater thant two!")
+    
+print()
