52d7d0a607aabd7d02374bc8b8c3ea4ce3053e47
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-09 17:29:59 -0300

    Update commenting.py

--- language-basics/commenting.py
+++ language-basics/commenting.py
@@ -1,1 +1,9 @@
+#Font: https://www.w3schools.com/python/python_comments.asp
 
+print("#This is a coment")
+print("Hello, World!")
+
+print("Hello, World! #This is a coment")#This is a coment
+
+print("#print('Hello, World!')")
+print("Cheers, Mate!")
