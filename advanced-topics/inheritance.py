315f6a7f7e5e4d09457e254770024aecba183c07
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-14 09:23:31 -0300

    Update inheritance.py

--- advanced-topics/inheritance.py
+++ advanced-topics/inheritance.py
@@ -1,91 +1,25 @@
-# Font: https://www.w3schools.com/python/python_datatypes.asp
+#Font: https://www.w3schools.com/python/python_inheritance.asp
 
-#int
-x = 5
-print(type(x))
+print("#Creating a Parent Class")
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+        
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+x = Person("John","Doe")
+x.printname()
 print()
 
-#string
-x = "Hello World"
-print(type(x))
-print()
+print("#Creating a Child Class")
 
-#float
-x = 20.5
-print(type(x))
-print()
+class Student(Person):
+    pass
 
-#complex
-x = 1j
-print(type(x))
-print()
+y = Student("John","Doe")
+y.printname()
 
-#dict
-z = {"fruit" : "mango", "price" : 5.0}
-print(type(x))
-print()
-
-#list
-x = ["apple", "banana", "cherry"]
-print(type(x))
-x[1] = "mango"
-x.append("lemon")
-print(x)
-print(x.__sizeof__())
-print()
-
-#tuple
-x = ("apple", "banana", "cherry")
-print(type(x))
-print(id(x))
-x += ("lemon",) # We can't update an element, but we can add
-print(x.__sizeof__()) # tuples size is smaller than list
-print(id(x)) # here a new tuple object will be created
-print(x)
-z[x] = "fruits list" # We can't use list as key in a dictionary, only typle 
-print(z)
-print()
-
-#range
-x = range(6)
-print(type(x))
-print(list(x))
-print()
-
-#set
-x = {"apple", "banana", "cherry"}
-print(type(x))
-x.add("mango")
-print(x)
-print()
-
-#frozenset
-x = frozenset({"apple", "banana", "cherry"}) # To make set immutable
-print(type(x))
-print(x)
-print()
-
-#bool
-x = True
-print(type(x))
-x = False
-print(x)
-print()
-
-#bytearray
-x = bytearray(5)
-print(type(x))
-print(x)
-print()
-
-#bytes
-x = b"Hello" # Immutable version of bytearray
-print(type(x))
-print(x)
-print()
-
-#memoryview
-x = memoryview(bytes(5))
-print(type(x))
-print(x)
 print()
