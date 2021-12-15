25ab715dec3952ff564e3a8ec075b3cb66719fa1
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-15 13:15:22 -0300

    Update dunder.py

--- advanced-topics/dunder.py
+++ advanced-topics/dunder.py
@@ -1,91 +1,66 @@
-# Font: https://www.w3schools.com/python/python_datatypes.asp
+#Font: https://www.section.io/engineering-education/dunder-methods-python
 
-#int
-x = 5
-print(type(x))
-print()
+print("#Dunder")
 
-#string
-x = "Hello World"
-print(type(x))
-print()
+class Software: 
+    names = []
+    versions = {}
 
-#float
-x = 20.5
-print(type(x))
-print()
+    def __init__(self, names):
+        if names:
+            self.names = names.copy()
+            for name in names:
+                self.versions[name] = 1
+        else:
+            raise Exception("Please Enter the names")
+            
+    def __str__(self):
+        s = "The current softwares and their versions are listed below: \n"
+        for key,value in self.versions.items():
+            s+= f"{key} : v{value} \n"
+        return s
 
-#complex
-x = 1j
-print(type(x))
-print()
+    def __setitem__(self,name,version):
+        if name in self.versions:
+            self.versions[name] = version
+        else:
+            raise Exception("Software Name doesn't exist")
+            
+    def __getitem__(self,name):
+        if name in self.versions:
+            return self.versions[name]
+        else:
+            raise Exception("Software Name doesn't exist")
+            
+    def __delitem__(self,name):
+        if name in self.versions:
+            del self.versions[name]
+            self.names.remove(name)
+        else:
+            raise Exception("Software Name doent exist")
+            
+    def __len__(self):
+        return len(self.names)
+        
+    def __contains__(self,name):
+        if name in self.versions:
+            return True
+        else:
+            return False
+    
+p = Software(['S1', 'S2', 'S3'])
+p['S2'] = 2
+del p['S1']
+#p['2'] = 2 Will raise an exception
+print(p['S2'])
+print(len(p))
 
-#dict
-z = {"fruit" : "mango", "price" : 5.0}
-print(type(x))
-print()
+if 'S2' in p:
+    print("Software Exists")
+else:
+    print("Software DOESN'T exist")
 
-#list
-x = ["apple", "banana", "cherry"]
-print(type(x))
-x[1] = "mango"
-x.append("lemon")
-print(x)
-print(x.__sizeof__())
-print()
+print(p)
+#p1 = Software([]) Will raise an exception
 
-#tuple
-x = ("apple", "banana", "cherry")
-print(type(x))
-print(id(x))
-x += ("lemon",) # We can't update an element, but we can add
-print(x.__sizeof__()) # tuples size is smaller than list
-print(id(x)) # here a new tuple object will be created
-print(x)
-z[x] = "fruits list" # We can't use list as key in a dictionary, only typle 
-print(z)
-print()
-
-#range
-x = range(6)
-print(type(x))
-print(list(x))
-print()
-
-#set
-x = {"apple", "banana", "cherry"}
-print(type(x))
-x.add("mango")
-print(x)
-print()
-
-#frozenset
-x = frozenset({"apple", "banana", "cherry"}) # To make set immutable
-print(type(x))
-print(x)
-print()
-
-#bool
-x = True
-print(type(x))
-x = False
-print(x)
-print()
-
-#bytearray
-x = bytearray(5)
-print(type(x))
-print(x)
-print()
-
-#bytes
-x = b"Hello" # Immutable version of bytearray
-print(type(x))
-print(x)
-print()
-
-#memoryview
-x = memoryview(bytes(5))
-print(type(x))
-print(x)
 print()
