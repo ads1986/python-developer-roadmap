219fcd208ec0b8838f9b95c33a3a95a4b95baede
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-22 18:03:52 -0300

    Update module_custom.py

--- advanced-topics/module_custom.py
+++ advanced-topics/module_custom.py
@@ -1,1 +1,28 @@
+#Font: https://www.w3schools.com/python/python_modules.asp
 
+print("#Create a module")
+print()
+
+def greeting(name):
+    print("Hello, " + name)
+
+import module_custom
+
+module_custom.greeting("Anderson")
+
+print()
+
+print("#Variables in module")
+print()
+
+    
+person1 = {
+    "name" : "John",
+    "age" : 36,
+    "country" : "Norway"
+}
+    
+a = module_custom.person1["age"]
+print(a)
+
+print()
