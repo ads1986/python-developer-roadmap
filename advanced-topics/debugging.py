afe7c8bc31a94633c9802d0fcf0dd056ed86d2fd
Anderson Martins <anderson.dsmartins@outlook.com>
2021-12-22 12:05:20 -0300

    Update debugging.py

--- advanced-topics/debugging.py
+++ advanced-topics/debugging.py
@@ -1,1 +1,16 @@
+#Font: https://www.w3schools.com/python/python_iterators.asp
 
+print("#Debugging")
+print()
+
+import pdb
+
+print("#PDB")
+
+pdb.set_trace()
+
+n=5
+for x in range(1,11):
+    print(n, '*', x, '=', n*x)
+
+print()
